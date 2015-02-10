e8f1108f101e34d51b345920c6b66b99d7cddd43
jslopeza <jasonlopeza95@gmail.com>
2015-02-10 11:20:39 -0500

    Completed step 2, vote for an office

--- meteor-demo.js
+++ meteor-demo.js
@@ -1,10 +1,41 @@
 Officers = new Meteor.Collection('office');
 
 if (Meteor.isClient) {
-    Template.officers
-.helpers({
+    Template.officers.helpers({
         officers: function() {
-            return Officers.find({});
+            return Officers.find({}, {
+                sort: {
+                    score: -1,
+                    name: 1
+                }
+            });
+        },
+
+    });
+
+    Template.officer.helpers({
+        selectedClass: function() {
+            var playerId = this._id;
+            var selectedPlayer = Session.get('selectedplayer');
+            if (selectedPlayer === playerId) {
+                return 'selected';
+            }
+        }
+    });
+
+    Template.officers.events({
+        'click .officer-name': function(e, tmpl) {
+            Session.set('selectedplayer', this._id);
+        },
+        'click button': function(e, tmpl) {
+            var id = Session.get('selectedplayer');
+            Officers.update({
+                _id: id
+            }, {
+                $inc: {
+                    score: 1
+                }
+            });
         }
     });
 }
@@ -14,27 +45,33 @@ if (Meteor.isServer) {
         Officers.remove({});
         Officers.insert({
             name: 'Mohammad',
-            position: 'President'
+            position: 'President',
+            score: 0
         });
         Officers.insert({
             name: 'Eddo',
-            position: 'Vice President'
+            position: 'Vice President',
+            score: 0
         });
         Officers.insert({
             name: 'Anthony',
-            position: 'Treasurer'
+            position: 'Treasurer',
+            score: 0
         });
         Officers.insert({
             name: 'Theresa',
-            position: 'Secretary'
+            position: 'Secretary',
+            score: 0
         });
         Officers.insert({
             name: 'Matt',
-            position: 'Web Master'
+            position: 'Web Master',
+            score: 0
         });
         Officers.insert({
             name: 'Al',
-            position: 'Public Relations'
+            position: 'Public Relations',
+            score: 0
         });
     });
 }
