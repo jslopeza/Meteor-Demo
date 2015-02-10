ac5fb18abe1c4fa1a32b437ee67ec446e49e097a
jslopeza <jasonlopeza95@gmail.com>
2015-02-10 10:26:33 -0500

    Step one, display all the officers from mongo

--- meteor-demo.js
+++ meteor-demo.js
@@ -1,23 +1,40 @@
+Officers = new Meteor.Collection('office');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    Template.officers
+.helpers({
+        officers: function() {
+            return Officers.find({});
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+        Officers.remove({});
+        Officers.insert({
+            name: 'Mohammad',
+            position: 'President'
+        });
+        Officers.insert({
+            name: 'Eddo',
+            position: 'Vice President'
+        });
+        Officers.insert({
+            name: 'Anthony',
+            position: 'Treasurer'
+        });
+        Officers.insert({
+            name: 'Theresa',
+            position: 'Secretary'
+        });
+        Officers.insert({
+            name: 'Matt',
+            position: 'Web Master'
+        });
+        Officers.insert({
+            name: 'Al',
+            position: 'Public Relations'
+        });
+    });
 }
